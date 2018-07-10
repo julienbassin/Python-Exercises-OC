7b50d9771f86f512c5b168b80f4defe56e003eff
julien bassin <julienbassin@outlook.com>
2018-07-10 22:52:40 +0200

    code poussé

--- ZCasino.py
+++ ZCasino.py
@@ -1,16 +1,41 @@
-import random
+from random import randrange
+from math import ceil
 
-utiliser les assert python et s assurer de rentrer un nombre entier 
-utiliser random pour récupérer un nombre aléatoire 
-arrondir le nombre si la mise est gagnée (ceil method)
-le joueur mise entre 0 et 49 
-    depose de l'argent donc checker si joueur a argent
-roulette possède 50 cases 
-    numero pair sont noirs et impairs sont rouges
-si le numero gagnant == numero mise:
-    alors joueur gagne (probabilité 1/50)
-sinon :
-    si numero_mise est pair:
-        joueur recupere 50% de la mise
-    sinon numero est impair:
-        joueur perd sa mise
+Amount = 1000
+Continue_party = True
+bet_number = -1
+bet = 0 
+
+print("You will start a part with", Amount, "$")
+while Continue_party:
+    while bet_number < 0 or bet_number > 49:
+        bet_number = input("Enter a number between 0 and 49 ?")
+        try:
+            bet_number = (int)bet_number
+        except ValueError:
+            print("This is not a number")
+            bet_number = -1
+            continue
+    while bet < 0  or bet => Amount:
+        bet = input(" how much do ?")
+        try:
+            bet = int(bet)
+        except ValueError:
+            print("this is not a number")
+            bet = -1
+            continue
+    magic_number = randrange(50)
+
+    if magic_number == bet_number:
+        Amount = bet * 3
+    elif magic_number % 2 == bet_number % 2:
+        Amount = (ceil)bet * 0.5
+    else:
+        print("you lost your bet")
+        Amount = Amount - bet
+    
+    if Amount < 0:
+        print("no more money , you have to leave !")
+        Continue_party = False
+    else:
+        
